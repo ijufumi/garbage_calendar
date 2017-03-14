802b76435eb3812dc79f49e1ffad6427de57c404
ijufumi <ijufumi@gmail.com>
2017-03-14 15:45:42 +0900

    Update README.md

---
+++ garbage_calendar/garbage_calendarPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace garbage_calendar
+{
+	public partial class garbage_calendarPage : ContentPage
+	{
+		public garbage_calendarPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
