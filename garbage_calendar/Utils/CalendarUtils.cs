9698fdf69c1ffbb909ed582d064181189eb29403
Takafumi Iju <takafumi.iju@sk.com>
2017-03-29 16:51:58 +0900

    Add method of calculate prev/next month days.

--- garbage_calendar/Utils/CalendarUtils.cs
+++ garbage_calendar/Utils/CalendarUtils.cs
@@ -6,9 +6,7 @@ namespace garbage_calendar.Utils
     {
         public static int CalcRowSize(int year, int month)
         {
-            var date = new DateTime(year, month, 1);
-            var startWeek = date.DayOfWeek;
-            var diff = startWeek - DayOfWeek.Sunday;
+            var diff = CalcPrevMonthDays(year, month);
             var endOfDay = DateTime.DaysInMonth(year, month);
             var cellCountWithPrefix = diff + endOfDay;
             if (cellCountWithPrefix % 7 == 0)
@@ -17,6 +15,25 @@ namespace garbage_calendar.Utils
             }
 
             return cellCountWithPrefix / 7 + 1;
+        }
+
+        public static int CalcPrevMonthDays(int year, int month)
+        {
+            var date = new DateTime(year, month, 1);
+            var startWeek = date.DayOfWeek;
+            return startWeek - DayOfWeek.Sunday;
+        }
+
+        public static int CalcNextMonthDays(int year, int month)
+        {
+            var date = new DateTime(year, month, 1);
+            var endOfDay = DateTime.DaysInMonth(year, month);
+            date = new DateTime(year, month, endOfDay);
+
+            var startWeek = date.DayOfWeek;
+
+            return DayOfWeek.Saturday - startWeek;
+
         }
     }
 }
