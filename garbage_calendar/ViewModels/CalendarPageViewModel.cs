90025b8a2afbe1fc25de474bb8be78e662dc7f1c
Takafumi Iju <takafumi.iju@sk.com>
2017-04-05 14:05:10 +0900

    [WIP]Add page navigation process.

--- garbage_calendar/ViewModels/CalendarPageViewModel.cs
+++ garbage_calendar/ViewModels/CalendarPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using Prism.Commands;
 using Prism.Mvvm;
@@ -6,9 +7,13 @@ using Prism.Navigation;
 
 namespace garbage_calendar.ViewModels
 {
-    public class CalendarPageViewModel : BindableBase
+    public class CalendarPageViewModel : BindableBase, INavigatingAware
     {
         private INavigationService _navigationService;
+
+        private int _year;
+        private int _month;
+
         public CalendarPageViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
@@ -39,10 +44,24 @@ namespace garbage_calendar.ViewModels
 
         async Task ShowNextMonthAsync(string month)
         {
-            _navigationService.NavigateAsync("CalendarPage");
+            var dateTime = new DateTime(_year, _month, 1);
+            dateTime = dateTime.AddMonths(1);
+            var parameters = new NavigationParameters();
+
+            parameters.Add("year", dateTime.Year);
+            parameters.Add("month", dateTime.Month);
+
+            _navigationService.NavigateAsync("CalendarPage", parameters);
         }
         async Task ShowPrevMonthAsync(string month)
         {
+            var dateTime = new DateTime(_year, _month, 1);
+            dateTime = dateTime.AddMonths(-1);
+            var parameters = new NavigationParameters();
+
+            parameters.Add("year", dateTime.Year);
+            parameters.Add("month", dateTime.Month);
+
             _navigationService.NavigateAsync("CalendarPage");
         }
         async Task CellClickAsync(int? day)
@@ -54,6 +73,17 @@ namespace garbage_calendar.ViewModels
         private bool CanShowPrev => true;
         private bool CanCellClick => true;
 
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("year"))
+            {
+                _year = (int) parameters["year"];
+            }
+            if (parameters.ContainsKey("month"))
+            {
+                _month = (int) parameters["month"];
+            }
+        }
     }
 
 }
