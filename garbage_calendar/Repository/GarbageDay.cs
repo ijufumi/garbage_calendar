6023fdea80e0a12ddbf92a0241aab2a3a11c187b
Takafumi Iju <takafumi.iju@sk.com>
2017-04-28 00:29:48 +0900

    [WIP] Add offline sync.

--- garbage_calendar/Repository/GarbageDay.cs
+++ garbage_calendar/Repository/GarbageDay.cs
@@ -1,7 +1,17 @@
-﻿namespace garbage_calendar.Repository
+﻿using System;
+
+namespace garbage_calendar.Repository
 {
     public class GarbageDay
     {
+        public int Year { get; set; }
 
+        public int Month { get; set; }
+
+        public int Type { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
     }
 }
